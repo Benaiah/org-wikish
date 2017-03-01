e515523dfa9ac104a23a690a5c084552824ff24e
Benaiah Mischenko <benaiah@mischenko.com>
2017-03-01 01:18:32 -0800

    Link relatively; create files before entering

--- org-wikish.el
+++ org-wikish.el
@@ -22,14 +22,32 @@
 (defun org-wikish-file-name-to-path (filename)
   (concat (file-name-as-directory org-wikish-notes-directory) filename))
 
+(defun org-wikish-file-name (path)
+  (concat (file-name-base path) (file-name-extension path)))
+
+(defun org-wikish-maybe-relative-path (path &optional buffer-path)
+  (let ((buffer-path (if buffer-path buffer-path buffer-file-name)))
+    (if (and (file-name-absolute-p path)
+             buffer-path
+             (org-wikish-in-wiki-p buffer-path))
+        (let ((newpath (file-relative-name path org-wikish-notes-directory)))
+          (concat "./" newpath))
+      path)))
+
 (defun org-wikish-word-to-path (word)
-  (concat (file-name-as-directory org-wikish-notes-directory)
-          (org-wikish-word-to-filename word)))
+  (org-wikish-maybe-relative-path
+   (concat (file-name-as-directory org-wikish-notes-directory)
+           (org-wikish-word-to-filename word))))
 
 (defun org-wikish-word-to-link (word)
   (string-join (list "[[" (org-wikish-word-to-path word) "][" word "]]")))
 
-(defun org-wikish-replace-word-with-link ()
+(defun org-wikish-in-wiki-p (path)
+  (string-equal
+   (file-name-directory path)
+   (file-name-as-directory org-wikish-notes-directory)))
+
+(defun org-wikish-link-word-at-point ()
   (interactive)
   (let ((bounds (bounds-of-thing-at-point 'symbol)))
     (when bounds
@@ -37,12 +55,26 @@
              (filename (org-wikish-word-to-filename word))
              (link (org-wikish-word-to-link word)))
         (delete-region (car bounds) (cdr bounds))
-        (insert (org-wikish-word-to-link word))))))
+        (insert link)))))
 
-(defun org-wikish-replace-word-with-link-and-enter ()
+(defun org-wikish-ensure-page-exists (word)
+  (let ((path (org-wikish-word-to-path word)))
+    (if (not (file-exists-p path))
+        (write-region "" nil path))))
+
+(defun org-wikish-link-word-at-point-and-enter (&optional dont-create)
   (interactive)
-  (org-wikish-replace-word-with-link)
-  (org-open-at-point))
+  (let ((word (thing-at-point 'symbol)))
+    (org-wikish-link-word-at-point)
+    (if (not dont-create)
+        (org-wikish-ensure-page-exists word))
+    (org-open-at-point)))
+
+(defun org-wikish-open-link-at-point ()
+  (interactive)
+  (if (org-in-regexp org-bracket-link-regexp 1)
+      (org-open-at-point)
+    (org-wikish-link-word-at-point-and-enter)))
 
 (defun org-wikish-page-completion-list ()
   (mapcar (lambda (filename)
@@ -54,6 +86,8 @@
   (interactive (list (completing-read "Org file: " (org-wikish-page-completion-list))))
   (org-open-file (org-wikish-word-to-path name)))
 
-(define-key org-mode-map (kbd "C-c w g") #'org-wikish-replace-word-with-link)
-(define-key org-mode-map (kbd "C-c w G") #'org-wikish-replace-word-with-link-and-enter)
+(define-key org-mode-map (kbd "C-c w g") #'org-wikish-link-word-at-point)
+(define-key org-mode-map (kbd "C-c w G") #'org-wikish-link-word-at-point-and-enter)
+(define-key org-mode-map (kbd "C-c w o") #'org-wikish-open-link-at-point)
 (define-key global-map (kbd "C-c w f") #'org-wikish-find-page)
+
